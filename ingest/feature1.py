da5ef9c5ecfff7220d8e74a6e272b2e5e7de6f79
samsudin <samsudiney@gmail.com>
2023-07-09 11:51:04 +0700

    merevisi file feature1.py

--- ingest/feature1.py
+++ ingest/feature1.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 import matlplotlib
+import airflow
 
 data = pd.DataFrame([])
 print(data)
 
 proces = []
 process = data
+
+for data in proces:
+    print(data)
+
